38da1c5f156097e3b7099a763fb83d94301ce195
Marcin Bober <marcin@bober.pl>
2022-04-13 21:13:57 +0200

    Linked to test database

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI='mysql://22021:BiegoweSwiry22@localhost/22021_produkcja'
+SQLALCHEMY_DATABASE_URI='mysql://22021:BiegoweSwiry22@sportoweswiry.atthost24.pl/22021_SSTest'
 SQLALCHEMY_TRACK_MODIFICATIONS=False

4d8b77b459685d2b7d801a0d28ccda0560cd43ae
Marcin Bober <marcin@bober.pl>
2022-06-12 20:20:23 +0200

    google login

--- user/routes.py
+++ user/routes.py
@@ -475,7 +475,7 @@ def loginGoogle():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     GOOGLE_CLIENT_ID = '1038815102985-ijajop9lhj2djsoua450a1orfpsm463h.apps.googleusercontent.com'
     client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-    flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],redirect_uri="http://127.0.0.1:5000/callback")
+    flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],redirect_uri="http://test.sportoweswiry.atthost24.pl/callback")
 
 
     authorization_url, state = flow.authorization_url() 
@@ -492,7 +492,7 @@ def callbackGoogle():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     GOOGLE_CLIENT_ID = '1038815102985-ijajop9lhj2djsoua450a1orfpsm463h.apps.googleusercontent.com'
     client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-    flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],redirect_uri="http://127.0.0.1:5000/callback")
+    flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],redirect_uri="http://test.sportoweswiry.atthost24.pl//callback")
 
 
 

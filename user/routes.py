a7a843864bc0e71ecbb1a77d38b3ea6363dd6afd
LukBartsch <lukasz.bartsch@gmail.com>
2022-06-12 21:36:01 +0200

    New page view for contact form and sent message

--- user/routes.py
+++ user/routes.py
@@ -581,8 +581,8 @@ def callbackConnect():
 
 
 
-@user.route("/test", methods=['POST', 'GET'])
-def test():
+# @user.route("/test", methods=['POST', 'GET'])
+# def test():
 
-    form=UserForm()
-    return render_template("newNewUserForm.html", form=form)
+#     form=UserForm()
+#     return render_template("NEWsendMessage.html", form=form)

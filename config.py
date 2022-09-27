b075c54bc7f1863e2a7bf57ce987c1ee52490671
LukBartsch <lukasz.bartsch@gmail.com>
2022-09-27 20:45:37 +0200

    Added updated config function

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 SQLALCHEMY_DATABASE_URI='mysql://22021:BiegoweSwiry22@sportoweswiry.atthost24.pl/22021_SSTest'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
+SECRET_KEY='SportoweSwiry22'
+
+MAIL_SERVER='sportoweswiry.atthost24.pl'
+MAIL_PORT=465
+MAIL_USERNAME='admin@sportoweswiry.atthost24.pl'
+MAIL_PASSWORD='BiegoweSwiry22'
+MAIL_USE_TLS=False
+MAIL_USE_SSL=True
+MAIL_DEFAULT_SENDER='Sportowe Świry <admin@sportoweswiry.atthost24.pl>'
+
+FB_CLIENT_ID='427488192540443'
--- start.py
+++ start.py
@@ -13,25 +13,7 @@ def create_app():
     app.config.from_pyfile('config.py')
     migrate = Migrate(app, db)
 
-    app.config['SECRET_KEY'] ='SportoweSwiry22'
-
-    #app.config['MAIL_SERVER']='smtp.gmail.com'
-    #app.config['MAIL_PORT'] = 587
-    #app.config['MAIL_USERNAME'] = 'sportowe.swiry.app@gmail.com'
-    #app.config['MAIL_PASSWORD'] = 'BiegoweSwiry22'
-    #app.config['MAIL_USE_TLS'] = True
-    #app.config['MAIL_USE_SSL'] = False
-    #app.config['MAIL_DEFAULT_SENDER'] = 'sportowe.swiry.app@gmail.com'
-
-
-    app.config['MAIL_SERVER']='sportoweswiry.atthost24.pl'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'admin@sportoweswiry.atthost24.pl'
-    app.config['MAIL_PASSWORD'] = 'BiegoweSwiry22'
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_DEFAULT_SENDER'] = 'Sportowe Świry <admin@sportoweswiry.atthost24.pl>'
-
+    app.config['FLASK_APP']='main'
     app.config['AVATARS_SAVE_PATH'] = os.path.join(app.static_folder, 'avatars')
 
     db.init_app(app)
